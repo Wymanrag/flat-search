0769570eeedde5bb0cc9bdb424a1f96f72729593
Wymanrag <ze.pedro.rodrigues@gmail.com>
2016-05-11 01:37:08 +0100

    introduced logging

--- flat-search.py
+++ flat-search.py
@@ -2,12 +2,13 @@
 #flat-serach.py v1
 #
 #Search for houses on some sites
+
 #START
 import ConfigParser
 import subprocess
 import json
-
-print "STARTED...\n"
+import hashlib
+import logging
 
 # def cleanOLX(j2clean):
 #  	nr=0
@@ -30,38 +31,43 @@ def cleanOLX(j2clean):
  		if "all" in a:
  			j2clean_aux.append(a)
  	return j2clean_aux
-#READ .conf
-# with open('importioapi.conf') as f:
-# 	lines = f.readlines()
-#lines = [line.rstrip('\n') for line in open('importioapi.conf')]
-parser = ConfigParser.ConfigParser ()
-parser.read('settings.conf')
 
-user_guid = parser.get('OLX', 'USER_GUID')
-urlencoded_api_key = parser.get('OLX', 'urlencoded_api_key')
-extractor_guid = parser.get('OLX', 'extractor_guid')
-#print ("user_guid = %s\nurlencoded_api_key = %s\nextractor_guid = %s" % (user_guid, urlencoded_api_key, extractor_guid))
-
-#run API bash
-#rc = subprocess.call("./bashtractor.sh %s %s %s urls.txt data.json", shell = True)
-
-#parse JSON
-jsonfile = open('data.json.bkp',  'rb')
-jdata = json.load(jsonfile)
-jsonfile.close()
-
-#clean OLX null objects
-jdata = cleanOLX(jdata)
-for a in jdata:
-	print a["all"]
-#print jdata[0]["all"].encode('utf-8')
-#print jdata[1]["all"].encode('utf-8')
-
-
-# for i in jdata["outputProperties"]:
-# 	print i["name"]
+def main():
+	logging.basicConfig(level=logging.INFO)
 	
-#create aged DB..
+	logging.info('STARTED...')
+	#READ .conf
+	# with open('importioapi.conf') as f:
+	# 	lines = f.readlines()
+	#lines = [line.rstrip('\n') for line in open('importioapi.conf')]
+	parser = ConfigParser.ConfigParser ()
+	parser.read('settings.conf')
 
-print "\n...DONE"
+	user_guid = parser.get('OLX', 'USER_GUID')
+	urlencoded_api_key = parser.get('OLX', 'urlencoded_api_key')
+	extractor_guid = parser.get('OLX', 'extractor_guid')
+	#print ("user_guid = %s\nurlencoded_api_key = %s\nextractor_guid = %s" % (user_guid, urlencoded_api_key, extractor_guid))
 
+	#run API bash
+	#rc = subprocess.call("./bashtractor.sh %s %s %s urls.txt data.json", shell = True)
+
+	#parse JSON
+	jsonfile = open('data.json.bkp',  'rb')
+	jdata = json.load(jsonfile)
+	jsonfile.close()
+
+	#clean OLX null objects
+	jdata = cleanOLX(jdata)
+	for a in jdata:
+		logging.debug(a["all"])
+	#print jdata[0]["all"].encode('utf-8')
+	#print jdata[1]["all"].encode('utf-8')
+
+	#create aged DB..
+	hash_object = hashlib.md5(jdata[0]["all"].encode('utf-8')+jdata[0]["preco"].encode('utf-8'))
+	logging.debug(hash_object.hexdigest())
+
+	logging.info('...DONE')
+
+if __name__ == "__main__":
+    main()
