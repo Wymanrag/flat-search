d4c81a472db1e946b8b56dd7027337e6eb482ab3
Wymanrag <ze.pedro.rodrigues@gmail.com>
2016-05-14 01:39:42 +0100

    got multi url working but not email

--- flat-search.py
+++ flat-search.py
@@ -27,14 +27,23 @@ def create_newapart_list(newapart):
 
 
 def queryAPI(userguid, apikey, extractor, url):
+	#return json with apartments
 	headers = {'Content-Type': 'application/json',}
 	data = '{"input":{"webpage/url":"%s"}}' %(url)
 	logging.debug("data is "+data)
 	posturl = ("https://api.import.io/store/connector/%s/_query?_user=%s&_apikey=%s" % (extractor, userguid, apikey))
-	rc = requests.post(posturl, headers=headers, data=data)
-	logging.info("request status_code: %d" % (rc.status_code))
-	logging.debug(rc.url)
-	return rc
+	
+	results = {}
+	counter1 = 0
+	while "results" not in results:
+		counter1 += 1
+		rc = requests.post(posturl, headers=headers, data=data)
+		logging.info("request status_code: %d" % (rc.status_code))
+		results = rc.json()
+		if counter1 is 5:
+			logging.info("UPS...Could retrieve data from %s" % (rc.url) )
+			break
+	return results
 
 def send_email(user, pwd, recipient, subject, body):
     import smtplib
@@ -43,9 +52,10 @@ def send_email(user, pwd, recipient, subject, body):
     gmail_pwd = pwd
     FROM = user
     TO = recipient if type(recipient) is list else [recipient]
+    print type(TO)
     SUBJECT = subject
     TEXT = body.encode('utf8')
-
+    
     # Prepare actual message
     message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
     """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
@@ -60,47 +70,86 @@ def send_email(user, pwd, recipient, subject, body):
     except:
         logging.info("failed to send mail")
 
-def search_OLX(configs):
+def db_init():
+	dictaux = {}
+	if os.path.isfile('dbhash.json'): 
+		jdbfile = open ('dbhash.json', 'r')
+		jdbfilesize = os.stat('dbhash.json').st_size
+		logging.debug("dbhash.json SIZE = %d", jdbfilesize)
+		if jdbfilesize != 0:
+			jdbinit = json.load(jdbfile)
+			jdbfile.close()
+			logging.debug(jdbinit)
+		else:
+			jdbinit = dictaux
+	else:
+		jdbinit = dictaux
+	logging.info("db has %d entries!", len(jdbinit))
+	return jdbinit
+
+def db_update(dbref):
+	jdbfile = open ('dbhash.json', 'wb')
+	json.dump(dbref, jdbfile)
+	logging.info("dumped jdb to file")
+	logging.debug(jdbfile)
+	jdbfile.close()
+
+
+def db_clean_old():
+	pass
+
+def search4apart(configs):
 	user_guid = configs.get('IMPORT_IO', 'your_user_guid')
 	urlencoded_api_key = configs.get('IMPORT_IO', 'your_urlencoded_api_key')
-	extractor_guid = configs.get('OLX', 'extractor_guid')
-	logging.debug ("user_guid = %s\nurlencoded_api_key = %s\nextractor_guid = %s" % (user_guid, urlencoded_api_key, extractor_guid))
+	#extractor_guid = configs.get('OLX', 'extractor_guid')
+	#logging.debug ("user_guid = %s\nurlencoded_api_key = %s\nextractor_guid = %s" % (user_guid, urlencoded_api_key, extractor_guid))
 	sites_supported = ["OLX", "IMOVIRTUAL", "BPI", "TESTE"]
 
+	#initiate database
+	jdb = db_init()
+
+	#CORE engine
+	jdata = {}
+	newstuff = []
 	for site in sites_supported:
 		logging.warning(site)
 		urllist = json.loads(configs.get(site, 'url'))
 		logging.warning(urllist)
 		if urllist: 
 			for url in urllist:
-				print url
-	exit("Exiting for debug")
+				logging.info("Now will get data from url: %s" % (url))
+				extractor_guid = configs.get('API_EXTRACTORS', site)
+				logging.warning("extractor_guid = %s " % extractor_guid)
+				rc = queryAPI(user_guid, urlencoded_api_key, extractor_guid, url)
+				jdata = rc
 
-	jdata = {}
-	#if not url:
-	#	for eachurl in url:
-		#call import IO API
-	counter1 = 1
-	while "results" not in jdata:
-		logging.info("...Quering import.io for OLX...")
-		rc = queryAPI(user_guid, urlencoded_api_key, extractor_guid, url)
-		jdata = rc.json()
-		counter1 += 1
-		#stop if tried 5 times
-		if counter1 is 5:
-			logging.info("UPS...Could retrieve data from %s" % (rc.url) )
-			break
+				if "results" in jdata:
+					jdata = cleanOLX(jdata)
+				else:
+					logging.info("UPS...no results in json")
+					logging.warning(jdata)
+
+				for obj in jdata:
+				#hash_object = hashlib.md5(jdata[0]["all"].encode('utf-8')+jdata[0]["preco"].encode('utf-8'))
+					hash_object = hashlib.md5(obj["all"].encode('utf-8')+obj["preco"].encode('utf-8'))
+					#logging.warning(hash_object.hexdigest())
+					dictaux = {hash_object.hexdigest():"date"}
+					if hash_object.hexdigest() not in jdb:
+						jdb.update(dictaux)
+						logging.debug(hash_object.hexdigest())
+						newstuff.append(obj)
+	db_update(jdb)
+	#exit("Exiting for debug")
 
 
 	#clean OLX null objects
-	if "results" in jdata:
-		jdata = cleanOLX(jdata)
-	else:
-		logging.info("UPS...no results in json")
-		logging.warning(jdata)
+	# if "results" in jdata:
+	# 	jdata = cleanOLX(jdata)
+	# else:
+	# 	logging.info("UPS...no results in json")
+	# 	logging.warning(jdata)
 
-	return jdata
-
+	return newstuff
 
 def main():
 	logging.basicConfig(level=logging.INFO)
@@ -115,42 +164,42 @@ def main():
 
 
 	#call search_OLX to sera
-	jdata = search_OLX(parser)
+	newstuff = search4apart(parser)
 
 	#
 	#create aged DB..
 	#
-	dictaux = {}
-	newstuff = []
+	# dictaux = {}
+	# newstuff = []
 	#check if db exists, else initiate it
-	if os.path.isfile('dbhash.json'): 
-		jdbfile = open ('dbhash.json', 'r')
-		jdbfilesize = os.stat('dbhash.json').st_size
-		logging.debug("dbhash.json SIZE = %d", jdbfilesize)
-		jdb = json.load(jdbfile)
-		jdbfile.close()
-		logging.debug(jdb)
-	else:
-		jdb = dictaux
+	# if os.path.isfile('dbhash.json'): 
+	# 	jdbfile = open ('dbhash.json', 'r')
+	# 	jdbfilesize = os.stat('dbhash.json').st_size
+	# 	logging.debug("dbhash.json SIZE = %d", jdbfilesize)
+	# 	jdb = json.load(jdbfile)
+	# 	jdbfile.close()
+	# 	logging.debug(jdb)
+	# else:
+	# 	jdb = dictaux
 
-	for obj in jdata:
-		#hash_object = hashlib.md5(jdata[0]["all"].encode('utf-8')+jdata[0]["preco"].encode('utf-8'))
-		hash_object = hashlib.md5(obj["all"].encode('utf-8')+obj["preco"].encode('utf-8'))
-		#logging.warning(hash_object.hexdigest())
-		dictaux = {hash_object.hexdigest():"date"}
-		if hash_object.hexdigest() not in jdb:
-			jdb.update(dictaux)
-			logging.debug(hash_object.hexdigest())
-			newstuff.append(obj)
+	# for obj in jdata:
+	# 	#hash_object = hashlib.md5(jdata[0]["all"].encode('utf-8')+jdata[0]["preco"].encode('utf-8'))
+	# 	hash_object = hashlib.md5(obj["all"].encode('utf-8')+obj["preco"].encode('utf-8'))
+	# 	#logging.warning(hash_object.hexdigest())
+	# 	dictaux = {hash_object.hexdigest():"date"}
+	# 	if hash_object.hexdigest() not in jdb:
+	# 		jdb.update(dictaux)
+	# 		logging.debug(hash_object.hexdigest())
+	# 		newstuff.append(obj)
 
-	logging.info("db has %d entries!", len(jdb))
+
 	logging.info("newstuff has %d entries!", len(newstuff))
 
-	jdbfile = open ('dbhash.json', 'wb')
-	json.dump(jdb, jdbfile)
-	logging.info("dumped jdb to file")
-	logging.debug(jdbfile)
-	jdbfile.close()
+	# jdbfile = open ('dbhash.json', 'wb')
+	# json.dump(jdb, jdbfile)
+	# logging.info("dumped jdb to file")
+	# logging.debug(jdbfile)
+	# jdbfile.close()
 
 	newaparts = create_newapart_list(newstuff)
 	body = "\n".join(newaparts)
